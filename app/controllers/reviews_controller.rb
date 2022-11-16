89b894faf75fedbcea67703a260a97b47d69766b
Marcus Rocha <sif.marcusvinicius@gmail.com>
2022-11-16 15:44:54 -0300

    finish yelp

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,23 @@
+class ReviewsController < ApplicationController
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = Restaurant.find(params[:restaurant_id])
+    if @review.save
+      redirect_to restaurant_path(@review.restaurant)
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to restaurant_path(@review.restaurant), status: :see_other
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
+
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+10.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: (Restaurant::CATEGORIES).sample
+  )
+end
+
+
+200.times do
+  Review.create!(
+    rating: rand(0..5),
+    content: Faker::ChuckNorris.fact,
+    restaurant: Restaurant.all.sample
+  )
+end
